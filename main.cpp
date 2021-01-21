b280325e2dddb30c1a7cd2b81c8a633f4b15b128
Vladimir Shubarin <vhundef@gmail.com>
2021-01-21 17:58:56 +0300

    Fixed camera movement. It's now smooth
    Took 2 hours 5 minutes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "mesh.hpp"
 #include "renderer.hpp"
 #include "shader.hpp"
+#include "plane.h"
 
 LightsManager *lightsManager;
 float lastX = 0;
@@ -15,76 +16,81 @@ bool firstMouse = true;
 double deltaTime = 0.0f;// time between current frame and last frame
 double lastFrame = 0.0f;
 Camera *camera;
+int pressedKey = -1;
 
 void programQuit([[maybe_unused]] int key, [[maybe_unused]] int action, Application *app) {
-  app->close();
-  LOG_S(INFO) << "Quiting...";
+    app->close();
+    LOG_S(INFO) << "Quiting...";
 }
 
 void wasdKeyPress([[maybe_unused]] int key, [[maybe_unused]] int action, [[maybe_unused]] Application *app) {
-  if ((key == GLFW_KEY_W) && (action == GLFW_PRESS || action == GLFW_REPEAT))
-	camera->ProcessKeyboard(FORWARD, (float)deltaTime);
-  if ((key == GLFW_KEY_S) && (action == GLFW_PRESS || action == GLFW_REPEAT))
-	camera->ProcessKeyboard(BACKWARD, (float)deltaTime);
-  if ((key == GLFW_KEY_A) && (action == GLFW_PRESS || action == GLFW_REPEAT))
-	camera->ProcessKeyboard(LEFT, (float)deltaTime);
-  if ((key == GLFW_KEY_D) && (action == GLFW_PRESS || action == GLFW_REPEAT))
-	camera->ProcessKeyboard(RIGHT, (float)deltaTime);
+    if (action == GLFW_PRESS) { pressedKey = key; }
+    if (action == GLFW_RELEASE) { pressedKey = -1; }
+}
+
+void moveCamera() {
+    if (pressedKey == GLFW_KEY_W) { camera->ProcessKeyboard(FORWARD, (float) deltaTime); }
+    if (pressedKey == GLFW_KEY_S) { camera->ProcessKeyboard(BACKWARD, (float) deltaTime); }
+    if (pressedKey == GLFW_KEY_A) { camera->ProcessKeyboard(LEFT, (float) deltaTime); }
+    if (pressedKey == GLFW_KEY_D) { camera->ProcessKeyboard(RIGHT, (float) deltaTime); }
 }
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
 void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
-  if (firstMouse) {
-	lastX = (float)xpos;
-	lastY = (float)ypos;
-	firstMouse = false;
-  }
+    if (firstMouse) {
+        lastX = (float) xpos;
+        lastY = (float) ypos;
+        firstMouse = false;
+    }
 
-  double xoffset = xpos - lastX;
-  double yoffset = lastY - ypos;// reversed since y-coordinates go from bottom to top
+    double xoffset = xpos - lastX;
+    double yoffset = lastY - ypos;// reversed since y-coordinates go from bottom to top
 
-  lastX = (float)xpos;
-  lastY = (float)ypos;
+    lastX = (float) xpos;
+    lastY = (float) ypos;
 
-  camera->ProcessMouseMovement(xoffset, yoffset);
+    camera->ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
-  camera->ProcessMouseScroll(yoffset);
+    camera->ProcessMouseScroll(yoffset);
 }
 
 int main(int argc, char *argv[]) {
-  Application app({1280, 720}, argc, argv);
-  Application::setOpenGLFlags();
-  app.registerKeyCallback(GLFW_KEY_ESCAPE, programQuit);
+    Application app({1280, 720}, argc, argv);
+    Application::setOpenGLFlags();
+    app.registerKeyCallback(GLFW_KEY_ESCAPE, programQuit);
 
-  app.registerKeyCallback(GLFW_KEY_W, wasdKeyPress);
-  app.registerKeyCallback(GLFW_KEY_A, wasdKeyPress);
-  app.registerKeyCallback(GLFW_KEY_S, wasdKeyPress);
-  app.registerKeyCallback(GLFW_KEY_D, wasdKeyPress);
+    app.registerKeyCallback(GLFW_KEY_W, wasdKeyPress);
+    app.registerKeyCallback(GLFW_KEY_A, wasdKeyPress);
+    app.registerKeyCallback(GLFW_KEY_S, wasdKeyPress);
+    app.registerKeyCallback(GLFW_KEY_D, wasdKeyPress);
 
-  lastX = app.getWindow()->getWindowSize().x / 2.0f;
-  lastY = app.getWindow()->getWindowSize().y / 2.0f;
+    lastX = app.getWindow()->getWindowSize().x / 2.0f;
+    lastY = app.getWindow()->getWindowSize().y / 2.0f;
 
-  glDepthFunc(GL_LESS);
-  glCall(glPolygonMode(GL_FRONT_AND_BACK, GL_FILL));
+    glDepthFunc(GL_LESS);
+    glCall(glPolygonMode(GL_FRONT_AND_BACK, GL_FILL));
 
-  Shader shader_tex("../shaders/multiple_diffuse_shader_tex.glsl", false);
-  shader_tex.bind();
-  shader_tex.setUniform1i("u_Texture", 0);
-  shader_tex.setUniform1i("numDiffLights", 1);
-  std::vector<Mesh *> meshes;
+    Shader shader_tex("../shaders/multiple_diffuse_shader_tex.glsl", false);
+    shader_tex.bind();
+    shader_tex.setUniform1i("u_Texture", 0);
+    shader_tex.setUniform1i("numDiffLights", 1);
+    std::vector<Mesh *> meshes;
 
-  //базовая высота -2.5
-  Texture floor_metal_tex("../textures/floor/flat-floor4_8.png");
-  Texture floor_blue_tex("../textures/floor/flat-flat14.png");
-  Texture pillar_brown_tex("../textures/brown1.png");
+    //базовая высота -2.5
+    Texture floor_metal_tex("../textures/floor/flat-floor4_8.png");
+    Texture floor_blue_tex("../textures/floor/flat-flat14.png");
+    Texture pillar_brown_tex("../textures/brown1.png");
 
-  Mesh cube("../resources/models/Crate1.obj");
+    Mesh cube("../resources/models/Crate1.obj");
+    Plane plane({0, 0, 0}, {0, 0, -10}, {10, 0, -10}, {10, 0, 0});
 
+    plane.addTexture("../textures/floor/flat-floor4_8.png")->setPosition({1, 1, 1})->setScale({1, 1, 1})->compile();
+/*
   for (int i = 0; i < 10; ++i) {
 	for (int j = 0; j < 13; ++j) {
 	  if ((i == 3 || i == 8) && (j == 3 || j == 7)) {
@@ -108,37 +114,39 @@ int main(int argc, char *argv[]) {
 		meshes.back()->setTextures({&floor_metal_tex})->setPosition({i * 2, -2.5, j * 2})->setScale({1, 0.3, 1})->compile();
 	  }
 	}
-  }
+  }*/
 
-  lightsManager = new LightsManager;
-  lightsManager->addLight(DiffuseLight("1_1", {{0, 10, 0}, {0.8, 0.8, 0.8}, 0.8}));
+    lightsManager = new LightsManager;
+    lightsManager->addLight(DiffuseLight("1_1", {{0, 10, 0}, {0.8, 0.8, 0.8}, 0.8}));
 
-  // camera
-  camera = new Camera(glm::vec3(0.0f, 0.0f, 6.0f));
-  camera->setWindowSize(app.getWindow()->getWindowSize());
+    // camera
+    camera = new Camera(glm::vec3(0.0f, 0.0f, 6.0f));
+    camera->setWindowSize(app.getWindow()->getWindowSize());
 
-  glfwSetCursorPosCallback(app.getWindow()->getGLFWWindow(), mouse_callback);
-  glfwSetScrollCallback(app.getWindow()->getGLFWWindow(), scroll_callback);
-  lightsManager->getLightByNameDir("1_1")->enable();
+    glfwSetCursorPosCallback(app.getWindow()->getGLFWWindow(), mouse_callback);
+    glfwSetScrollCallback(app.getWindow()->getGLFWWindow(), scroll_callback);
+    lightsManager->getLightByNameDir("1_1")->enable();
 
-  while (!app.getShouldClose()) {
-	app.getWindow()->updateFpsCounter();
-	auto currentFrame = glfwGetTime();
-	deltaTime = currentFrame - lastFrame;
-	lastFrame = currentFrame;
-	//updating data for shader
-	shader_tex.reload();
-	Renderer::clear({0, 0, 0, 1});
+    while (!app.getShouldClose()) {
+        app.getWindow()->updateFpsCounter();
+        auto currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        moveCamera();
+        //updating data for shader
+        shader_tex.reload();
+        Renderer::clear({1, 0, 0, 1});
 
-	camera->passDataToShader(&shader_tex);
-	lightsManager->passDataToShader(&shader_tex);
-	for (auto &mesh : meshes) {
-	  mesh->draw(&shader_tex);
-	}
+        camera->passDataToShader(&shader_tex);
+        lightsManager->passDataToShader(&shader_tex);
+        plane.draw(&shader_tex);
+        /*for (auto &mesh : meshes) {
+          mesh->draw(&shader_tex);
+        }*/
 
-	glCall(glfwSwapBuffers(app.getWindow()->getGLFWWindow()));
-	glfwPollEvents();
-  }
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
+        glCall(glfwSwapBuffers(app.getWindow()->getGLFWWindow()));
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
